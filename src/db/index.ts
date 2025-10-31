a7d4fc764703e5387753e805005cce1116832096
bhaveshsinghal95182 <work.bhavesh22@gmail.com>
2025-10-31 10:20:24 +0530

    db setup with neon and drizzle

---
+++ src/db/index.ts
@@ -0,0 +1,8 @@
+import { drizzle } from 'drizzle-orm/neon-http';
+import 'dotenv/config';
+import {eq} from "drizzle-orm";
+import { usersTable } from "./schema";
+
+const db = drizzle(process.env.DATABASE_URL!);
+
+
